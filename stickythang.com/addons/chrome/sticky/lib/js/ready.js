abf0f7c7db9207367dc47e807ddfa19e5f087de1
Patrick Clancey <clanceyp@gmail.com>
2011-12-29 17:41:10 +0000

    update from lil'kim

--- stickythang.com/addons/chrome/sticky/lib/js/ready.js
+++ stickythang.com/addons/chrome/sticky/lib/js/ready.js
@@ -2,22 +2,21 @@
 (function(){
 	
 YUI_config = {
-    ignore : [ "querystring-stringify-simple" ]
+   //  ignore : [ "querystring-stringify-simple" ]
 };
 		
 	if (!YUI){
 		console.log('ooch yui not loaded')
 		return;
+	}else{
+		console.log('yui loaded')
+		YUI().use('node','node-load','dd-plugin','resize','json','transition', function(Y) {
+	
+			stickythang.Y = Y;
+		    stickythang.init();
+			
+		});
 	}
-	YUI().use('node', function(Y) {
-
-	    function init() {
-	        // The DOM is ready, lets say hello!
-	        stickythang.init();
-	    }
-	 
-	    Y.on("domready", init); 
-	});
 	
 
 	
