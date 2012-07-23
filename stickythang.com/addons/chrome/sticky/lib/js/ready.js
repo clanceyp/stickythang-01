30986c485ad0aa78686247774670b73ac54ecf51
Patrick Clancey <clanceyp@gmail.com>
2012-07-23 17:38:38 +0100

    Update to manifest 2 and move server to go daddy

--- stickythang.com/addons/chrome/sticky/lib/js/ready.js
+++ stickythang.com/addons/chrome/sticky/lib/js/ready.js
@@ -21,4 +21,17 @@ YUI_config = {
 })()
 
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33017882-1']);
+_gaq.push(['_trackPageview']);
+
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
 
